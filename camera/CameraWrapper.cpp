9c756704f1f69329de23569e6a315f88ac8ae7eb
Hamster Tian <th0928@126.com>
2014-09-23 22:27:23 +0800

    CameraWrapper: add support for metering mode
    Change-Id: Ic192c7fd545759d2d6beb039aba64b60abdb0e21

--- camera/CameraWrapper.cpp
+++ camera/CameraWrapper.cpp
@@ -33,6 +33,8 @@
 #include <camera/Camera.h>
 #include <camera/CameraParameters.h>
 
+static char KEY_AUTO_EXPOSURE[] = "auto-exposure";
+static char KEY_SUPPORTED_AUTO_EXPOSURE[] = "auto-exposure-values";
 static char KEY_SUPPORTED_ISO_MODES[] = "iso-values";
 static char KEY_ISO_MODE[] = "iso";
 
@@ -47,12 +49,16 @@ static char KEY_SONY_ISO_AVAIL_MODES[] = "sony-iso-values";
 static char KEY_SONY_ISO_MODE[] = "sony-iso";
 static char KEY_SONY_AE_MODE_VALUES[] = "sony-ae-mode-values";
 static char KEY_SONY_AE_MODE[] = "sony-ae-mode";
+static char KEY_SONY_METERING_MODE_VALUES[] = "sony-metering-mode-values";
+static char KEY_SONY_METERING_MODE[] = "sony-metering-mode";
 
 // Sony parameter values
 static char VALUE_SONY_ON[] = "on";
 static char VALUE_SONY_OFF[] = "off";
 static char VALUE_SONY_STILL_HDR[] = "on-still-hdr";
+static char VALUE_SONY_AUTO_EXPOSURE_SPOT[] = "spot";
 static char VALUE_SONY_INTELLIGENT_ACTIVE[] = "on-intelligent-active";
+static char VALUE_AUTO_EXPOSURE_SPOT_METERING[] = "spot-metering";
 
 static android::Mutex gCameraWrapperLock;
 static camera_module_t *gVendorModule = 0;
@@ -159,6 +165,23 @@ static char *camera_fixup_getparams(int id, const char *settings)
         params.set(KEY_SUPPORTED_ISO_MODES, buffer);
     }
 
+    if (params.get(KEY_SONY_METERING_MODE_VALUES)) {
+        const char *meteringModeList = params.get(KEY_SONY_METERING_MODE_VALUES);
+
+        char buffer[255] = "";
+        strcpy(buffer, meteringModeList);
+
+        char* spotPtr = strstr(meteringModeList, VALUE_SONY_AUTO_EXPOSURE_SPOT);
+        if (spotPtr != NULL) {
+            char spotLoc = spotPtr - meteringModeList;
+            buffer[spotLoc] = 0;
+            strcat(buffer, VALUE_AUTO_EXPOSURE_SPOT_METERING);
+            strcat(buffer, spotPtr + 4);
+        }
+
+        params.set(KEY_SUPPORTED_AUTO_EXPOSURE, buffer);
+    }
+
     if (params.get(KEY_SONY_IMAGE_STABILISER)) {
         const char *sony_is = params.get(KEY_SONY_IMAGE_STABILISER);
         if (strcmp(sony_is, VALUE_SONY_STILL_HDR) == 0) {
@@ -203,6 +226,15 @@ static char *camera_fixup_getparams(int id, const char *settings)
                 params.set(KEY_ISO_MODE, "auto");
                 params.set("shutter-speed","auto");
             }
+        }
+    }
+
+    if (params.get(KEY_SONY_METERING_MODE) && params.get(KEY_SONY_METERING_MODE_VALUES)) {
+        const char* meteringMode = params.get(KEY_SONY_METERING_MODE);
+        if (strcmp(meteringMode, VALUE_SONY_AUTO_EXPOSURE_SPOT) == 0) {
+            params.set(KEY_AUTO_EXPOSURE, VALUE_AUTO_EXPOSURE_SPOT_METERING);
+        } else {
+            params.set(KEY_AUTO_EXPOSURE, meteringMode);
         }
     }
 
@@ -288,6 +320,14 @@ static char *camera_fixup_setparams(int id, const char *settings)
             }
             params.set(KEY_SONY_IMAGE_STABILISER, VALUE_SONY_OFF);
         }
+    }
+
+    if (params.get(KEY_AUTO_EXPOSURE) && params.get(KEY_SONY_METERING_MODE)) {
+       if (strcmp(params.get(KEY_AUTO_EXPOSURE), VALUE_AUTO_EXPOSURE_SPOT_METERING) == 0) {
+           params.set(KEY_SONY_METERING_MODE, VALUE_SONY_AUTO_EXPOSURE_SPOT);
+       } else {
+           params.set(KEY_SONY_METERING_MODE, params.get(KEY_AUTO_EXPOSURE));
+       }
     }
 
 #if !LOG_NDEBUG
